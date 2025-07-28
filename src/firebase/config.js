0ae444d7afea74d96ff843c0dac8196fa556b9b3
Angeline Halim <halimangeline1@gmail.com>
2025-07-28 13:12:02 +0800

    Fix: vendor auth, routing, and profile handling updates, listing

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,9 +2,9 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+import { getStorage } from "firebase/storage";
 
-// Your Firebase config
+// Replace with your Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyCPhPbubzWs6rA_zDkSpiPPsczzIbci8SQ",
   authDomain: "ripple-83a69.firebaseapp.com",
@@ -18,9 +18,12 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase services
+// Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+
+// Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
-export const storage = getStorage(app);
 
 export default app;
+
+export const storage = getStorage(app);
