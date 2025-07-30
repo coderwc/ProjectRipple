af5ebc0519e31da7c7a7db4040fcc8eaf297f89c
Angeline Halim <halimangeline1@gmail.com>
2025-07-30 11:46:26 +0800

    WIP: save changes before switching to main

--- src/components/vendor/pages/AddListing.js
+++ src/components/vendor/pages/AddListing.js
@@ -160,7 +160,7 @@ const AddListing = ({ user, onBack }) => {
           <div>
             <label className="block text-sm font-medium text-blue-700 mb-3">Item Condition</label>
             <div className="grid grid-cols-3 gap-2">
-              {['New', 'Used', 'B-Grade'].map((condition) => (
+              {['New', 'Used', 'Imperfect'].map((condition) => (
                 <button
                   key={condition}
                   type="button"
